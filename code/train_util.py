c2e4de568a66bf4caeba8d9304c15ff172bf809e
Wei Liu (刘伟) <44130920+jugechengzi@users.noreply.github.com>
2023-07-01 10:20:56 +0800

    Update train_util.py

--- code/train_util.py
+++ code/train_util.py
@@ -241,34 +241,6 @@ def train_noshare(model, optimizer, dataset, device, args):
     return precision, recall, f1_score, accuracy
 
 
-def train_skew(model, optimizer, dataset, device, args):
-    TP = 0
-    TN = 0
-    FN = 0
-    FP = 0
-    for (batch, (inputs, masks, labels)) in enumerate(dataset):
-        optimizer.zero_grad()
-        inputs, masks, labels = inputs.to(device), masks.to(device), labels.to(device)
-        logits=model.train_skew(inputs,masks,labels)
-        cls_loss = args.cls_lambda * F.cross_entropy(logits, labels)
-        cls_loss.backward()
-        optimizer.step()
-        cls_soft_logits = torch.softmax(logits, dim=-1)
-        _, pred = torch.max(cls_soft_logits, dim=-1)
-
-        # TP predict 和 label 同时为1
-        TP += ((pred == 1) & (labels == 1)).cpu().sum()
-        # TN predict 和 label 同时为0
-        TN += ((pred == 0) & (labels == 0)).cpu().sum()
-        # FN predict 0 label 1
-        FN += ((pred == 0) & (labels == 1)).cpu().sum()
-        # FP predict 1 label 0
-        FP += ((pred == 1) & (labels == 0)).cpu().sum()
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    f1_score = 2 * recall * precision / (recall + precision)
-    accuracy = (TP + TN) / (TP + TN + FP + FN)
-    return precision, recall, f1_score, accuracy
 
 
 def train_g_skew(model, optimizer, dataset, device, args):
